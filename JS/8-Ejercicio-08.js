a7ff9cb959b8c2842caaf69919345581bb598f12
Alejo3D <alejandro_insinga@hotmail.com>
2025-04-15 21:08:39 -0300

    Ejercicio 8 terminado

---
+++ JS/8-Ejercicio-08.js
@@ -0,0 +1,7 @@
+let num = parseInt(prompt("Ingresa un número:"));
+
+if (num % 2 === 0) {
+  document.writeln("El " + num + " es divisible por 2.");
+} else {
+  document.writeln("El " + num + " no es divisible por 2.");
+}
